a43888371eb04ef3757d5054ac1915e306a18824
AKANSHI SRIVASTAVA <akanshi32@gmail.com>
2017-04-29 22:56:32 +0530

    Graph Implementation

--- app/src/main/java/com/akanshi/babymonitortm/LoginActivity.java
+++ app/src/main/java/com/akanshi/babymonitortm/LoginActivity.java
@@ -1,13 +1,29 @@
 package com.akanshi.babymonitortm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button loginbtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        loginbtn = (Button) findViewById(R.id.btnlogin);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
     }
 }
--- app/src/main/java/com/akanshi/babymonitortm/MainActivity.java
+++ app/src/main/java/com/akanshi/babymonitortm/MainActivity.java
@@ -2,16 +2,49 @@ package com.akanshi.babymonitortm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 import com.google.firebase.analytics.FirebaseAnalytics;
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class MainActivity extends AppCompatActivity {
 
     private FirebaseAnalytics mFirebaseAnalytics;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+        WebView webview = (WebView) findViewById(R.id.webview);
+
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        LineGraphSeries<DataPoint> series = new LineGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, 1),
+                new DataPoint(1, 5),
+                new DataPoint(2, 3),
+                new DataPoint(3, 2),
+                new DataPoint(4, 6)
+        });
+        graph.addSeries(series);
+
+        webview.setWebViewClient(new WebViewClient());
+        webview.getSettings().setJavaScriptEnabled(true);
+        webview.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+
+        webview.getSettings().setPluginState(WebSettings.PluginState.ON);
+        webview.getSettings().setMediaPlaybackRequiresUserGesture(false);
+        webview.setWebChromeClient(new WebChromeClient());
+        webview.loadUrl("192.168.43.83:8080");
+        mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
+
+       
     }
 }
